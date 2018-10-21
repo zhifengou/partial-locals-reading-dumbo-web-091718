d02ad8e67f47f0d88898612195d2b19e40727201
zhifeng ou <zhifengou@hotmail.com>
2018-10-21 23:07:58 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
     @author = @post.author
-    
+    @post = Post.find(params[:id])
   end
 
   def index
